ea2904297bf0a555a155d5eaaedb36eb1379640e
kinddfull <kinddfull@gmail.com>
2018-09-21 17:25:13 +0900

    handle async rejection

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -45,6 +45,7 @@ export default class serverConfig {
   }
 
   private connectServer(server: Http.Server) {
+    console.log('connect')
     this.server = server
     this.server.on('request', this.requestCallback)
   }
@@ -53,13 +54,17 @@ export default class serverConfig {
     const verify_token = query['hub.verify_token']
     const mode = query['hub.mode']
     const challenge = query['hub.challenge']
+    console.log('get verify token', verify_token)
+    console.log('input verify token', this.verifyToken)
     if (mode === 'subscribe' && verify_token === this.verifyToken) {
       res.writeHead(200)
       res.end(challenge)
+      console.log('challenge', challenge)
       return
     }
     res.writeHead(403)
     if (verify_token) {
+      console.warn(`${verify_token} is not verify token`)
       res.end(`${verify_token} is not verify token`)
     }
     res.end('verify token not found')
@@ -67,21 +72,30 @@ export default class serverConfig {
   private async requestPOST(req: Http.ServerRequest, res: Http.ServerResponse) {
     req.setEncoding('utf-8')
     let body
-    await req.on('data', chunk => {
-      body = JSON.parse(chunk)
-    })
-    const { object, entry } = body
-    if (object === 'page') {
-      entry.forEach(entry => {
-        const webhook_event = entry.messaging[0]
-        //handle webhook event
+    try {
+      await req.on('data', chunk => {
+        body = JSON.parse(chunk)
       })
+      const { object, entry } = body
+      if (object === 'page') {
+        await entry.forEach(entry => {
+          const webhook_event = entry.messaging[0]
+          //handle webhook event
+          console.log('webhook event', webhook_event)
+        })
 
-      res.statusCode = 200
-      res.end('event received')
-    } else {
-      res.statusCode = 404
-      res.end()
+        res.statusCode = 200
+        console.log('event received')
+        res.end('event received')
+      } else {
+        console.warn('object is not page')
+        res.end('object is not page')
+        res.statusCode = 404
+        res.end()
+      }
+    } catch (e) {
+      console.log('error', e.message)
+      res.end(e.message)
     }
   }
 
@@ -91,15 +105,17 @@ export default class serverConfig {
   ) => {
     const { method } = req
     const { path, query } = parseUrl(req.url)
-    console.log(this.webhook)
-    console.log(path)
     isSame(path, this.webhook)
       ? method === 'GET'
         ? this.requestGET(res, query)
         : method === 'POST'
           ? this.requestPOST(req, res)
-          : ((res.statusCode = 403), res.end(`${method} is not allowed method`))
-      : ((res.statusCode = 403), res.end(`${path} is not webhook url`))
+          : ((res.statusCode = 403),
+            console.warn(`${method} is not allowed method`),
+            res.end(`${method} is not allowed method`))
+      : ((res.statusCode = 403),
+        console.warn(`${path} is not webhook url`),
+        res.end(`${path} is not webhook url`))
   }
 
   listen(port: number & string) {
